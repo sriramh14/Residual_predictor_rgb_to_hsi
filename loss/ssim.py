38ee9cc86894bc75dc40c8d6be226e143876443c
sriramh14 <sriramhari1014@gmail.com>
2026-06-10 16:59:29 +0530

    Update ssim.py

--- loss/ssim.py
+++ loss/ssim.py
@@ -1,12 +1,15 @@
 import torchmetrics
 from torchmetrics.image import StructuralSimilarityIndexMeasure
-ssim_metric = (
-    StructuralSimilarityIndexMeasure(
-        data_range=1.0
-    ).cuda()
-)
+def ssim:
+    ssim_metric = (
+        StructuralSimilarityIndexMeasure(
+            data_range=1.0
+        ).cuda()
+    )
 
-ssim_value = ssim_metric(
-    pred,
-    gt
-)
+    ssim_value = ssim_metric(
+        pred,
+        gt
+    )
+    return ssim
+
